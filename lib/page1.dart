19601b7e2ba0a4a511fa9b4243939fabc8892555
naznincse00724105101089-tech <naznin.cse.00724105101089@aust.edu>
2026-09-14 22:07:21 +0600

    updated event page

--- lib/page1.dart
+++ lib/page1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'page3.dart';
 
 void main() {
@@ -13,75 +14,53 @@ class page1 extends StatefulWidget {
 }
 
 class _page1State extends State<page1> {
+  TextEditingController startController = TextEditingController();
 
-
-  TextEditingController startController =
-  TextEditingController();
-
-  TextEditingController destinationController =
-  TextEditingController();
-
+  TextEditingController destinationController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
   }
 
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-
       appBar: AppBar(
-        backgroundColor:  Color(0xFF070C16),
+        backgroundColor: Color(0xFF070C16),
         centerTitle: false,
 
-        title:  Text(
+        title: Text(
           'RouteWise',
-          style: TextStyle(
-            fontSize: 30,
-            color: Colors.white,
-          ),
+          style: TextStyle(fontSize: 30, color: Colors.white),
         ),
       ),
 
-
       body: Center(
-
         child: Column(
-
           mainAxisAlignment: .start,
 
           children: [
-
             SizedBox(height: 30),
 
-
             Container(
-
               width: double.infinity,
 
               decoration: BoxDecoration(
-                color:  Color(0xFF8A8CF6),
+                color: Color(0xFF8A8CF6),
 
                 borderRadius: BorderRadius.circular(9),
 
-                border: Border.all(
-                  color:  Color(0xFFBFE3f2),
-                ),
+                border: Border.all(color: Color(0xFFBFE3f2)),
               ),
 
-
               child: Column(
-
                 mainAxisAlignment: MainAxisAlignment.center,
 
                 crossAxisAlignment: CrossAxisAlignment.start,
 
                 children: [
-
-                   Text(
+                  Text(
                     'Where to next?',
 
                     style: TextStyle(
@@ -91,22 +70,15 @@ class _page1State extends State<page1> {
                     ),
                   ),
 
-
-                   SizedBox(height: 20),
-
-
+                  SizedBox(height: 20),
 
                   TextField(
-
                     controller: startController,
 
                     decoration: InputDecoration(
-
                       hintText: 'Start Location',
 
-                      hintStyle:  TextStyle(
-                        color: Colors.blueGrey,
-                      ),
+                      hintStyle: TextStyle(color: Colors.blueGrey),
 
                       prefixIcon: const Icon(
                         Icons.search,
@@ -120,29 +92,20 @@ class _page1State extends State<page1> {
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(7),
 
-                        borderSide: const BorderSide(
-                          color: Color(0xFF303747),
-                        ),
+                        borderSide: const BorderSide(color: Color(0xFF303747)),
                       ),
                     ),
                   ),
-
 
                   const SizedBox(height: 2),
 
-
-
                   TextField(
-
                     controller: destinationController,
 
                     decoration: InputDecoration(
-
                       hintText: 'Destination',
 
-                      hintStyle: const TextStyle(
-                        color: Colors.blueGrey,
-                      ),
+                      hintStyle: const TextStyle(color: Colors.blueGrey),
 
                       prefixIcon: const Icon(
                         Icons.search,
@@ -156,44 +119,30 @@ class _page1State extends State<page1> {
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(7),
 
-                        borderSide: const BorderSide(
-                          color: Color(0xFF303747),
-                        ),
+                        borderSide: const BorderSide(color: Color(0xFF303747)),
                       ),
                     ),
                   ),
 
-
                   SizedBox(height: 10),
 
-
-
                   ElevatedButton(
-
                     onPressed: () {
-
                       Navigator.push(
-
                         context,
 
                         MaterialPageRoute(
-
                           builder: (context) => Page3(
-
                             start: startController.text,
 
-                            destination:
-                            destinationController.text,
+                            destination: destinationController.text,
                           ),
                         ),
                       );
                     },
 
-                    child:  Text(
-                      "Search Bus",
-                    ),
+                    child: Text("Search Bus"),
                   ),
-
                 ],
               ),
             ),
@@ -203,5 +152,3 @@ class _page1State extends State<page1> {
     );
   }
 }
-
-

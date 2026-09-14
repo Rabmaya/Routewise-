c24284669821973721c2ee7269c6da34cb88ceaa
Rabmaya <rmehjabeenmim@gmail.com>
2026-09-14 17:22:48 +0600

    update everything

--- lib/page2.dart
+++ lib/page2.dart
@@ -150,6 +150,87 @@ class _page2State extends State<page2> {
               style: TextStyle(
                 color: Colors.white,fontSize: 18,
               ),
+              decoration: InputDecoration(
+                  hintText: 'Password:',
+                  hintStyle: const TextStyle(color: Color(0xFFAEB9CE),fontSize: 15
+                  ),
+                  filled: true,
+                  fillColor: const Color(0xFF151A24),
+                  suffixIcon: IconButton(icon: Icon(
+                    hidepassword1
+                        ?Icons.visibility_off
+                        :Icons.visibility,
+                  ),
+                      onPressed:()
+                      {
+                        setState(() {
+                          hidepassword1=!hidepassword1;
+                        });
+                      }
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(7),
+                    borderSide: const BorderSide(
+                      color:Color(0xFF303747),
+                    ),
+                  )
+              ),
+            ),
+            const SizedBox(height:10),
+            TextField(
+              obscureText: hidepassword,
+              style: TextStyle(
+                color: Colors.white,fontSize: 18,
+              ),
+              decoration: InputDecoration(
+                  hintText: 'Confirm Password:',
+                  hintStyle: const TextStyle(color: Color(0xFFAEB9CE),fontSize: 15
+                  ),
+                  filled: true,
+                  fillColor: const Color(0xFF151A24),
+                  suffixIcon: IconButton(icon: Icon(
+                    hidepassword
+                        ?Icons.visibility_off
+                        :Icons.visibility,
+                  ),
+                      onPressed:()
+                      {
+                        setState(() {
+                          hidepassword=!hidepassword;
+                        });
+                      }
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(7),
+                    borderSide: const BorderSide(
+                      color:Color(0xFF303747),
+                    ),
+                  )
+              ),
+            ),
+            const SizedBox(height:25),
+            SizedBox(
+                width: 150,
+                height:60,
+                child:ElevatedButton(onPressed:(){
+                  Navigator.push(context,
+                    MaterialPageRoute(builder: (context)=>const page1()
+                    ),
+                  );
+                },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF6264F2),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(7),
+                      ),
+                    ),
+                    child: const Text('Sign in',
+                      style: TextStyle(fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ))
+            ),
+          ],
 
 
         ),
